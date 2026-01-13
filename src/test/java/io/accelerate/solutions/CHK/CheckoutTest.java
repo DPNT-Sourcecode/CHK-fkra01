553a5ae3d0d21cd5f667f9c4638a857537496929
sbx_user1051 <sbx_user1051@169.254.18.113>
2026-01-13 18:14:21 +0000

    Tue Jan 13 18:14:21 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/CheckoutTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutTest.java
@@ -1,13 +1,22 @@
 package io.accelerate.solutions.CHK;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CheckoutTest {
   private CheckoutSolution solution = new CheckoutSolution();
 
   @BeforeEach
-  private void setupSolution() {
+  public void setupSolution() {
     solution = new CheckoutSolution();
   }
 
+  @ParameterizedTest
+  @CsvSource({"A,50", "B,30", "C,20", "AA,100", "BB,60", "CC,40", "AB,80", "ABC,100", "AAA,130", "BB,45", "AAAB,160", "AAABB,175", "AAABBC,195"})
+  public void testCheckout(String input, Integer expected) {
+
+  }
+
 }
+
