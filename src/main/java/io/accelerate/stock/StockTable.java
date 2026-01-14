81e6529d19f26f91bdb77eb9c8233fe27ea38227
sbx_user1051 <sbx_user1051@169.254.3.107>
2026-01-14 15:38:58 +0000

    Wed Jan 14 15:38:58 UTC 2026

--- src/main/java/io/accelerate/stock/StockTable.java
+++ src/main/java/io/accelerate/stock/StockTable.java
@@ -3,6 +3,7 @@ package io.accelerate.stock;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StockTable {
   HashMap<String, StockItem> items = new HashMap<>();
@@ -16,16 +17,35 @@ public class StockTable {
     items.put(item.sku, item);
   }
 
+  public List<String> getProcessingOrder() {
+    List<String> out = new ArrayList<>();
+    List<String> dependents = new ArrayList<>();
+    this.items.values()
+        .stream()
+        .map((x) -> x.dependents)
+        .filter((x) -> x != null).forEach(dependents::addAll);
+    var independents = this.items
+        .values()
+        .stream()
+        .filter((x) -> !dependents.contains(x))
+        .map((x) -> x.getSku())
+        .collect(Collectors.toList());
+    out.addAll(independents);
+    out.addAll(dependents);
+    return out;
+
+  }
+
   // Default factory method for the given stock table - likely wouldnt exist in a
   // real project
   public static StockTable defaultTable() {
     var items = List.of(
 
-        new StockItem("A", 50, List.of(new SpecialOffer(3, "A", 130), new SpecialOffer(5, "A", 200))),
-        new StockItem("B", 30, List.of(new SpecialOffer(2, "B", 45))),
-        new StockItem("C", 20, null),
-        new StockItem("D", 15, null),
-        new StockItem("E", 40, List.of(new SpecialOffer(2, "B", 0)))
+        new StockItem("A", 50, List.of(new SpecialOffer(3, "A", 130), new SpecialOffer(5, "A", 200)), null),
+        new StockItem("B", 30, List.of(new SpecialOffer(2, "B", 45)), null),
+        new StockItem("C", 20, null, null),
+        new StockItem("D", 15, null, null),
+        new StockItem("E", 40, List.of(new SpecialOffer(2, "B", 0)), List.of("B"))
 
     );
     // Definitely cleaner ways to do this, but this isnt "production" code
@@ -37,3 +57,4 @@ public class StockTable {
 
   }
 }
+
