040136507113933690eeb19758ae1d2a2951f4bf
sbx_user1051 <sbx_user1051@169.254.3.107>
2026-01-14 16:48:39 +0000

    Wed Jan 14 16:48:39 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/CheckoutTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutTest.java
@@ -20,7 +20,8 @@ public class CheckoutTest {
       "AAABB,175", "AAABBC,195", "AAAAA,200", "AAAAAA,250", "EEB,80", "EE,80", "EEEEBB,160", "BEBEEE,160",
       "ABCDEABCDE,280", "EEEB,120", "ABCDECBAABCABBAAAEEAA,665", "F,10", "FF,20", "FFF,20", "FFFF,30", "FFFFF,40",
       "FFFFFF,40", "H,10", "HHHHH,45", "HHHHHHHHHH,80", "K,80", "KK,150", "KKK,230", "KKKK,300", "NNN,120", "NNNM,120",
-      "NNNMM,135" })
+      "NNNMM,135", "NNNNMM,175", "NNNNNNMM,240", "PPPPP,200", "PPPPPPPPPP,400", "PPPPPPPPP,400", "QQ,60", "QQQ,80",
+      "RRRQ,150", "RRRQQ,180", "RRRQQQ,210", "UUU,120", "UUUU,120" })
   public void testCheckout(String input, Integer expected) {
     var output = solution.checkout(input);
     assertEquals(expected, output);
@@ -34,3 +35,4 @@ public class CheckoutTest {
   }
 
 }
+
