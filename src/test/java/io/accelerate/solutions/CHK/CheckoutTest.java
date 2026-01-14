55da28ad2e1654c1363b7292a2ae997f680b3e8e
sbx_user1051 <sbx_user1051@169.254.3.107>
2026-01-14 19:22:24 +0000

    Wed Jan 14 19:22:24 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/CheckoutTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutTest.java
@@ -19,10 +19,10 @@ public class CheckoutTest {
   @CsvSource({ "A,50", "B,30", "C,20", "D,15", "AA,100", "BB,45", "CC,40", "AB,80", "ABC,100", "AAA,130", "AAAB,160",
       "AAABB,175", "AAABBC,195", "AAAAA,200", "AAAAAA,250", "EEB,80", "EE,80", "EEEEBB,160", "BEBEEE,160",
       "ABCDEABCDE,280", "EEEB,120", "ABCDECBAABCABBAAAEEAA,665", "F,10", "FF,20", "FFF,20", "FFFF,30", "FFFFF,40",
-      "FFFFFF,40", "H,10", "HHHHH,45", "HHHHHHHHHH,80", "K,80", "KK,150", "KKK,230", "KKKK,300", "NNN,120", "NNNM,120",
+      "FFFFFF,40", "H,10", "HHHHH,45", "HHHHHHHHHH,80", "K,70", "KK,120", "KKK,190", "KKKK,240", "NNN,120", "NNNM,120",
       "NNNMM,135", "NNNNMM,175", "NNNNNNMM,240", "PPPPP,200", "PPPPPPPPPP,400", "PPPPPPPPP,400", "QQ,60", "QQQ,80",
       "RRRQ,150", "RRRQQ,180", "RRRQQQ,210", "UUU,120", "UUUU,120", "UUUUUUUU,240", "STX,45", "STY,45", "STZ,45",
-      "TXY,45", "TXZ,45", "XYZ,45", "SXY,45", "SXZ,45", "TXZ,45", "SYZ,45", "TYZ,45", "STXY,65", "STXYZ,86",
+      "TXY,45", "TXZ,45", "XYZ,45", "SXY,45", "SXZ,45", "TXZ,45", "SYZ,45", "TYZ,45", "STXY,62", "STXYZ,82",
       "SSTXYZ,90", "SSSZ,65", "ZZZS,65", "STXZ,62"
   })
   public void testCheckout(String input, Integer expected) {
@@ -38,3 +38,4 @@ public class CheckoutTest {
   }
 
 }
+
